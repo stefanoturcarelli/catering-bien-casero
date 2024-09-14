0b1b74cb4e48e98844bac9266c95d9a9ec3d4819
Stefano <stefano.turcarelli@outlook.com>
2024-09-14 10:34:08 -0500

    Refactor Frequently Asked Questions into individual component

---
+++ catering-bien-casero-nextjs/app/components/FAQ/FAQ.tsx
@@ -0,0 +1,49 @@
+export const FAQ = () => {
+  return (
+    <section>
+      <h2> Preguntas frecuentes</h2>
+      <div>
+        <p>¿CÓMO HAGO MI PEDIDO?</p>
+        <p>
+          Contactanos por redes sociales, whatsapp o rellena el formulario de
+          contacto. Una vez que coordinamos, te pediremos una seña del 50% para
+          reservar tu pedido. El 50% restante se puede abonar el día de la
+          entrega.
+        </p>
+      </div>
+      <div>
+        <p>¿CON CUÁNTO TIEMPO DE ANTICIPACIÓN DEBO HACER MI PEDIDO?</p>
+        <p>
+          Los perniles con 72 hs de anticipación es suficiente. Los catering
+          personalizados al menos con una semana de anticipación. De todas
+          formas, te recomendamos agendar tu evento lo antes posible para poder
+          asegurarte disponibilidad de agenda.
+        </p>
+      </div>
+      <div>
+        <p>¿CÓMO PUEDO ABONAR?</p>
+        <p> Aceptamos efectivo, transferencia bancaria y Mercado Pago.</p>
+      </div>
+      <div>
+        <p>¿CÓMO FUNCIONA EL CUPÓN DE DESCUENTO?</p>
+        <p>
+          Por cada vez que respondas nuestra encuesta de calidad, te daremos un
+          cupón con un 10% de descuento para que lo utilices en tu próximo
+          pedido de catering. Es válido por dos meses una vez obtenido el cupón.
+        </p>
+      </div>
+      <div>
+        <p>¿HACEN ENVÍOS?</p>
+        <p>
+          Sí, realizamos envíos en Zona Oeste principalmente pero llegamos a
+          varios lugares dentro de Zona Sur, Norte y CABA. Cotizamos el envío
+          por whatsapp.
+        </p>
+      </div>
+      <div>
+        <p>¿HACEN FACTURA?</p>
+        <p>¡Si, por supuesto! Emitimos factura C.</p>
+      </div>
+    </section>
+  );
+};
