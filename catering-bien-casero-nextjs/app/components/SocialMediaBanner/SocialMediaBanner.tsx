c752537429dcdeb7d1be00be1113806926a5578e
Stefano <stefano.turcarelli@outlook.com>
2024-09-14 10:34:31 -0500

    Refactor SocialMediaBanner component

---
+++ catering-bien-casero-nextjs/app/components/SocialMediaBanner/SocialMediaBanner.tsx
@@ -0,0 +1,12 @@
+export const SocialMediaBanner = () => {
+  return (
+    <section>
+      <h2> Nuestras redes sociales</h2>
+      <p>¡Seguinos en nuestras redes!</p>
+      <ul>
+        <li>Instagram</li>
+        <li>Facebook</li>
+      </ul>
+    </section>
+  );
+};
