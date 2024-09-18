18e2350e599ff45fa1d4e3ddb62fe933b6e7e38c
Stefano <stefano.turcarelli@outlook.com>
2024-09-18 11:31:03 -0500

    Add AnalyticsPage component for displaying and managing analytics

---
+++ catering-bien-casero-nextjs/app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+export default function AnalyticsPage() {
+  return (
+    <section>
+      <h1>Analytics</h1>
+      <p>
+        View and manage all your analytics. You can also create new analytics
+        and update existing ones.
+      </p>
+    </section>
+  );
+}
