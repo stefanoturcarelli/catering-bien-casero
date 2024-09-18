c5ac7fe66764c4b8c2c19aab80aeab4b02cf3231
Stefano <stefano.turcarelli@outlook.com>
2024-09-18 11:30:30 -0500

    Add AboutPage component with responsive hero banner

---
+++ catering-bien-casero-nextjs/app/about/page.tsx
@@ -0,0 +1,34 @@
+import styles from "./About.module.css";
+import { Button } from "../components/Button/Button";
+import { OurChef } from "../components/OurChef/OurChef";
+import { Noe } from "../components/Noe/Noe";
+import { OurVision } from "../components/OurVision/OurVision";
+import { FAQ } from "../components/FAQ/FAQ";
+import { SocialMediaBanner } from "../components/SocialMediaBanner/SocialMediaBanner";
+import { VideosBanner } from "../components/VideosBanner/VideosBanner";
+import { Banner } from "../components/contact/Banner";
+
+export default function AboutPage() {
+  return (
+    <>
+      <section className={styles.aboutUsHeroBanner}>
+        <div className="container">
+          <div className={styles.aboutUsHeroBannerContainer}>
+            <h1 className={styles.aboutUsHeroBannerTitle}>
+              Acerca de nosotros
+            </h1>
+            <p className={styles.aboutUsHeroBannerSubtitle}>Lo que hacemos</p>
+            <Button href="/contact" title="¿Hablamos?" style="primary" />
+          </div>
+        </div>
+      </section>
+      <OurChef />
+      <Noe />
+      <OurVision />
+      <SocialMediaBanner />
+      <VideosBanner />
+      <FAQ />
+      <Banner />
+    </>
+  );
+}
